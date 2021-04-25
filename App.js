8b54e881d1568e2a183bf99692ab7f16bed2c4c0
Robbie Dorsey <airgo32@gmail.com>
2021-04-25 17:33:18 -0400

    Changed text for App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yooooooo My name is Robbie and I just made a change to The Loop!</Text>
       <StatusBar style="auto" />
     </View>
   );
