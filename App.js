1d842764a02997cb6f65ab84db7f06de2afb6542
Alec Chappell <alecclyde@gmail.com>
2021-04-27 18:59:48 -0400

    Reverted changes from presentation.

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello, world!</Text>
+      <Text style={styles.textFontSize}>Hello, world!</Text>
       <StatusBar style="auto" />
     </View>
   );
