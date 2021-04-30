051f44a6a260110a5d7a877ede0cf3981e147ff3
Alec Chappell <alecclyde@gmail.com>
2021-04-30 17:24:53 -0400

    Add logo to site

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from "./assets/The_Loop.jpg"
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.textFontSize}>Hello, world!</Text>
       <StatusBar style="auto" />
+      <img src={Logo} alt="The Loop Logo" height={500} width={500} />
     </View>
   );
 }
