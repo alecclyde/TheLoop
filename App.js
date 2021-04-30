45782d58dae04509ae85281d26b5562b96269dcf
Alec Chappell <alecclyde@gmail.com>
2021-04-30 17:33:11 -0400

    Dark background, updated font.

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Logo from "./assets/The_Loop.jpg"
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textFontSize}>Hello, world!</Text>
+      <Text style={styles.textFontSize}>Welcome to...</Text>
       <StatusBar style="auto" />
       <img src={Logo} alt="The Loop Logo" height={500} width={500} />
     </View>
@@ -16,11 +16,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
   textFontSize: {
     fontSize: 48,
+    color: '#fd6c11'
   }
 });
