a2a470bc66b2bcbc75c3f9163502ad15da520586
Caden Robertson <cr1350@messiah.edu>
2021-08-31 08:50:20 -0400

    test test 2

--- app.test.js
+++ app.test.js
@@ -3,9 +3,8 @@ import renderer from 'react-test-renderer';
 
 import App from './App';
 //test test
-describe('<App />', () => {
-  it('exists', () => {
-    const app = renderer.create(<App />).toJSON();
-    expect(app).toBe(app);
+describe('this test', () => {
+  it('works', () => {
+    expect(1).toBe(1);
   });
 });
