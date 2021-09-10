1355b541ffec97873d324681396bc0aac4407a01
Robbie Dorsey <airgo32@gmail.com>
2021-09-10 00:07:37 -0400

    updated login screen

--- screens/logIn.js
+++ screens/logIn.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import {
   View,
@@ -133,46 +132,3 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
 });
-=======
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { signIn } from '../shared/firebaseMethods';
-import { globalStyles } from '../styles/global';
-import * as yup from 'yup';
-import { Formik } from 'formik';
-
-const LoginSchema = yup.object({
-    email: yup
-        .string()
-        .email()
-        .required("Please enter your email"),
-    password: yup
-        .string()
-        .required("Please enter your password"),
-})
-
-
-export default function Login({ navigation }) {
-    return(
-        <SafeAreaView style={globalStyles.container}>
-            <View>
-                <FormIk
-                    initialValues={{email: '', password: ''}}
-                    validationSchema={LoginSchema}
-                    onSubmit={(values) => {signIn(values.email, values.password, navigation);}}
-                
-                >
-                {/* {(props) => (
-
-                )} */}
-
-                </FormIk>
-
-
-
-            </View>
-        </SafeAreaView>
-    );
-}
->>>>>>> be42370 (Working on login screen)
