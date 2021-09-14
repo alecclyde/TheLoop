bcdf2880b245401e48bcb2df14c9d2b057d0347f
Caden <cr1350@messiah.edu>
2021-09-14 15:23:40 -0400

    undo

--- shared/firebaseMethods.js
+++ shared/firebaseMethods.js
@@ -53,8 +53,7 @@ export async function loggingOut(navigation) {
 // Grabs a single user's data
 export async function getUserData(userID) {
   try {
-    const user = await firebase.firestore().collection('users').doc(userID).get()
-    
+    const user = await firebase.firestore().collection('users').doc(userID).get();
     return user.data();
 
   } catch (err) {
