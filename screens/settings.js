26aa7ea8378db9c7978bcc684ad241466a57f8b3
Jmtonnies <jmtonnies@gmail.com>
2021-09-29 19:33:08 -0400

    Profile Page Updated

--- screens/settings.js
+++ screens/settings.js
@@ -1,12 +1,16 @@
 import { Text, View } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { globalStyles } from "../styles/global";
 
 export default function Settings({ navigation }) {
     return (
+        <SafeAreaView style={globalStyles.container}>
         <View>
             <Text>
                 Settings
             </Text>
         </View>
+    </SafeAreaView>
     );
 }
