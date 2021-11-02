d3851a53d0a9ed273d0a8a1f2683a23ca22b7ebb
Caden <cr1350@messiah.edu>
2021-11-02 15:27:44 -0400

    redux is working

--- store/actions/eventActions.js
+++ store/actions/eventActions.js
@@ -5,16 +5,21 @@ import * as firebase from "firebase";
 export function addEvent(newEvent) {
     return async function addEventThunk(dispatch, getState){
         const currentUser = firebase.auth().currentUser;
-        const id = await generateUniqueFirestoreId();
 
         const event = firebase.firestore().collection("events").add({
-            id: id,
-            name: newEvent.eventName,
-            loop: newEvent.eventLoop,
-            address: newEvent.eventAddress,
-            creator: currentUser.uid,
-            datetime: firebase.firestore.Timestamp.fromMillis(newEvent.eventDateTime),
+            name: newEvent.name,
+            loop: newEvent.loop,
+            // creator: currentUser.uid, // DEPRECATED, start transitioning into creatorID
+            creatorID: currentUser.uid,
+            address: newEvent.address,
+            recurAutomatically: false,
+            recurFrequency: 1,
+            // datetime: firebase.firestore.Timestamp.fromMillis(data.startDateTime), // DEPRECATED, start transitioning to startDateTime
+            startDateTime: firebase.firestore.Timestamp.fromMillis(data.startDateTime),
+            endDateTime: firebase.firestore.Timestamp.fromMillis(data.startDateTime), // Will want to query for actual end dateTime later
+            creationTimestamp: firebase.firestore.Timestamp.now(),
             attendees: [currentUser.uid],
+            location: new firebase.firestore.GeoPoint(0, 0), // Temporary value, adjust when Alec/Caden finishes geolocation
         });
         dispatch({ type: ADD_EVENT, payload: event})
     }
