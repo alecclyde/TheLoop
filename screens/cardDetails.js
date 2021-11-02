57c14f2bbb30c1d5b9bc4261a3aac67fbdf4b740
Robbie Dorsey <airgo32@gmail.com>
2021-11-02 14:45:51 -0400

    Working on event posts

--- screens/cardDetails.js
+++ screens/cardDetails.js
@@ -1,27 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, Alert } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useState, useEffect } from "react";
+import { View, Text, Alert, Keyboard, ScrollView } from "react-native";
+import { globalStyles } from "../styles/global";
 // import Card from '../shared/card';
-import { StackActions } from '@react-navigation/routers';
+import { StackActions } from "@react-navigation/routers";
 import Icon from "react-native-vector-icons/FontAwesome";
-import moment from 'moment';
-import { Card, Button } from 'react-native-elements/';
+import moment from "moment";
+import { Card, Button, Divider, Input } from "react-native-elements/";
 import * as firebase from "firebase";
-import { TouchableOpacity } from 'react-native-gesture-handler';
-import { registerEvent, unregisterEvent } from '../shared/firebaseMethods';
+import {
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
+import {
+  registerEvent,
+  unregisterEvent,
+  getUserData,
+} from "../shared/firebaseMethods";
 
 //Called by favorite and personal when you click on a card to display the content
 export default function CardDetails({ navigation, route }) {
-
   const [eventAttendees, setEventAttendees] = useState([]);
-  const [eventCreator, setEventCreator] = useState({id: route.params?.creatorID, name: ""});
+  const [eventCreator, setEventCreator] = useState({
+    id: route.params?.creatorID,
+    name: "",
+  });
 
   const [eventName, setEventName] = useState(route.params?.name);
   const [eventLoop, setEventLoop] = useState(route.params?.loop || "");
-  const [eventDateTime, setEventDateTime] = useState(route.params?.startDateTime || "");
+  const [eventDateTime, setEventDateTime] = useState(
+    route.params?.startDateTime || ""
+  );
   const [eventAddress, setEventAddress] = useState("");
 
-  const [user, setUser] = useState();
+  const [userID, setUserID] = useState();
+  const [userName, setUserName] = useState();
   const [isAttending, setIsAttending] = useState(false);
   const [isCreator, setIsCreator] = useState(false);
 
@@ -29,129 +41,162 @@ export default function CardDetails({ navigation, route }) {
 
   const AuthStateChangedListener = (user) => {
     if (user) {
-      setUser(user.uid)
-      if (user.uid == route.params?.creatorID) setIsCreator(true)
-    }
-  }
+      getUserData(user.uid).then((userData) => {
+        setUserName(userData.firstName + " " + userData.lastName);
+      });
 
+      setUserID(user.uid);
+      if (user.uid == route.params?.creatorID) setIsCreator(true);
+    }
+  };
 
   const onResult = (QuerySnapshot) => {
     const eventData = QuerySnapshot.data();
 
-    setEventName(eventData.name)
-    setEventLoop(eventData.loop)
-    setEventDateTime(eventData.startDateTime.seconds)
-    setEventAddress(eventData.address)
+    setEventName(eventData.name);
+    setEventLoop(eventData.loop);
+    setEventDateTime(eventData.startDateTime.seconds);
+    setEventAddress(eventData.address);
     // setEventCreator({id: eventData.creator, name: eventCreator.name})
 
-    updateAttendeeList(eventData.attendees)
-
-  }
+    updateAttendeeList(eventData.attendees);
+  };
 
   const updateAttendeeList = (attendees) => {
+    eventAtens = eventAtens.filter((attendee) =>
+      attendees.includes(attendee.id)
+    );
 
-    eventAtens = eventAtens.filter((attendee) => attendees.includes(attendee.id))
+    setEventAttendees(eventAtens);
 
-    setEventAttendees(eventAtens)
-  
     attendees.forEach((attendee) => {
       // if the array of existing attendees doesn't include this possible new one
-      if (!eventAtens.some((elem) => elem.id == attendee)) {        
+      if (!eventAtens.some((elem) => elem.id == attendee)) {
+        firebase
+          .firestore()
+          .collection("users")
+          .doc(attendee)
+          .get()
+          .then((snap) => {
+            const attendeeName =
+              snap.data().firstName + " " + snap.data().lastName;
+            // console.log("new read: " + attendeeName)
 
-        firebase.firestore().collection("users").doc(attendee).get()
-        .then((snap) => {
-          
-          const attendeeName = snap.data().firstName + ' ' + snap.data().lastName;
-          // console.log("new read: " + attendeeName)
+            setEventAttendees((eventAttendees) => [
+              ...eventAttendees,
+              { id: snap.id, name: attendeeName },
+            ]);
+            eventAtens.push({ id: snap.id, name: attendeeName });
 
-          setEventAttendees((eventAttendees) => [...eventAttendees, {id: snap.id, name: attendeeName}])
-          eventAtens.push({id: snap.id, name: attendeeName})
-
-          // if current ID is the creator, set creator accordingly
-          if (snap.id == eventCreator.id) {
-            setEventCreator({id: eventCreator.id, name: attendeeName})
-          }
-        })
+            // if current ID is the creator, set creator accordingly
+            if (snap.id == eventCreator.id) {
+              setEventCreator({ id: eventCreator.id, name: attendeeName });
+            }
+          });
       }
-    })
-  }
+    });
+  };
 
   const onError = (error) => {
     console.log(error);
-  }
+  };
 
-      // This may be a better method for reading users from the database, if I can get it to work
+  // This may be a better method for reading users from the database, if I can get it to work
 
-      // firebase.firestore().collection("users").where(firebase.firestore.FieldPath.documentId(), "in", eventAttendees).get()
-      // .then((snap) => {
-      //   if (snap.exists) {
-      //     console.log("Poggers")
-      //   } else {
-      //     console.log("Not Poggers")
-      //     console.log()
-      //   }
-      // })
-
+  // firebase.firestore().collection("users").where(firebase.firestore.FieldPath.documentId(), "in", eventAttendees).get()
+  // .then((snap) => {
+  //   if (snap.exists) {
+  //     console.log("Poggers")
+  //   } else {
+  //     console.log("Not Poggers")
+  //     console.log()
+  //   }
+  // })
 
   // gets the logged in user until (hopefully) we get some redux action
   useEffect(() => {
-    const subscriber = firebase.auth().onAuthStateChanged(AuthStateChangedListener);
+    const subscriber = firebase
+      .auth()
+      .onAuthStateChanged(AuthStateChangedListener);
 
-    return subscriber
-
-  })
+    return subscriber;
+  });
 
   // creates the listener to allow realtime data
   useEffect(() => {
-    const subscriber = firebase.firestore().collection("events").doc(route.params?.id).onSnapshot(onResult, onError);
+    const subscriber = firebase
+      .firestore()
+      .collection("events")
+      .doc(route.params?.id)
+      .onSnapshot(onResult, onError);
 
-    return subscriber
-  }, [])
+    return subscriber;
+  }, []);
 
   // determines whether or not the logged in user is attending the selected event
   useEffect(() => {
-    if (eventAttendees.some((elem) => elem.id == user)) {
-      setIsAttending(true)
+    if (eventAttendees.some((elem) => elem.id == userID)) {
+      setIsAttending(true);
     } else {
-      setIsAttending(false)
+      setIsAttending(false);
     }
-  }, [eventAttendees, user])
+  }, [eventAttendees, userID]);
 
   return (
-    <View style={globalStyles.container}>
-      <Card>
-      
+    <View style={{flex: 1}}>
+      <View style={{padding: 20}}>
         <View style={globalStyles.rowContainer}>
+          <Text style={globalStyles.titleText}>{eventName}</Text>
+          <Icon
+            onPress={() => navigation.dispatch(StackActions.pop(1))}
+            name="arrow-left"
+            size={25}
+            style={{ position: "absolute", left: 1 }}
+          />
+        </View>
+        <Divider orientation="horizontal" />
+        <Text>Address: {eventAddress}</Text>
+        <Text>Creator: {eventCreator.name}</Text>
+        <Text>Date: {moment.unix(eventDateTime).format("MMMM Do, YYYY")}</Text>
+        <Text>Time: {moment.unix(eventDateTime).format("hh:mm A")}</Text>
+        <Text>Loop: {eventLoop}</Text>
 
-            <Card.Title style={globalStyles.titleText}>{ eventName }</Card.Title>
-            <Icon onPress={() => navigation.dispatch(StackActions.pop(1))} name='arrow-left' size={25} style={{position: 'absolute', left: 1}}/>
+        <Divider orientation="horizontal" />
 
-          </View>
-          <Card.Divider/>
-        <Text>Address: { eventAddress }</Text>
-        <Text>Creator: { eventCreator.name }</Text>
-        <Text>Date: { moment.unix(eventDateTime).format("MMMM Do, YYYY") }</Text>
-        <Text>Time: { moment.unix(eventDateTime).format("hh:mm A") }</Text>
-        <Text>Loop: { eventLoop }</Text>
-        <Card.Divider/>
-        <Card.Title style={globalStyles.titleText}>Attendees</Card.Title>
-        { eventAttendees.map((attendee) =>
-        <Text key={attendee.id}>{ attendee.name }</Text>
-
+        <Text style={globalStyles.titleText}>Attendees</Text>
+        {eventAttendees.map((attendee) => (
+          <Text key={attendee.id}>{attendee.name}</Text>
+        ))}
+        {!isCreator && (
+          <Button
+            title={!isAttending ? "Register" : "Unregister"}
+            onPress={() => {
+              if (!isAttending) {
+                registerEvent(route.params?.id, userID);
+              } else {
+                unregisterEvent(route.params?.id, userID);
+              }
+            }}
+          />
         )}
+        <Divider orientation="horizontal" style={{ paddingTop: 15 }} />
+      </View>
 
-      </Card>
-      
-      {!isCreator && <Button
-        title = {!isAttending ? "Register" : "Unregister"}
-        onPress = {() => {
-          if (!isAttending) {
-            registerEvent(route.params?.id, user)
-          } else {
-            unregisterEvent(route.params?.id, user)
-          }
-        }}
-      />}
+      <ScrollView
+        style={{ flex: 1, backgroundColor: "red" }}
+        keyboardShouldPersistTaps="handled"
+      >
+        <Card style={{ backgroundColor: isAttending ? "white" : "gray" }}>
+          <Input
+            placeholder={"Post in " + eventName}
+            disabled={!isAttending}
+            multiline={true}
+            onBlur={Keyboard.dismiss()}
+          />
+
+          <Button title="Post" disabled={!isAttending} />
+        </Card>
+      </ScrollView>
     </View>
   );
 }
