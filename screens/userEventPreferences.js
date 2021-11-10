59323ae80a36bbff7d71db9beb4967b4b247af8f
Alec Chappell <32017824+alecclyde@users.noreply.github.com>
2021-11-10 14:09:37 -0500

    Spread Checkboxes out across page

--- screens/userEventPreferences.js
+++ screens/userEventPreferences.js
@@ -2,7 +2,6 @@ import {
   ImageBackground,
   View,
   SafeAreaView,
-  Keyboard,
   StyleSheet,
   Alert,
 } from "react-native";
@@ -33,185 +32,199 @@ export default function UserEventPreferences({ navigation }) {
   return (
     <View
       style={{
-        borderColor: "orange",
+        borderColor: "#ffa835",
         borderWidth: 7,
         flex: 1,
         backgroundColor: "#F8F8F8",
       }}
     >
-      <SafeAreaView style={globalStyles.container}>
-        <View style={{ alignItems: "center" }}>
-          <Formik
-            initialValues={{
-              Sports: false,
-              Music: false,
-              Volunteer: false,
-              Game: false,
-              Social: false,
-              Arts: false,
-              Outdoors: false,
-              Academic: false,
-              Media: false,
-            }}
-            //validationSchema={setUserLoopsSchema}
-            onSubmit={(joinedLoops) => {
-              var success = setUserLoops({
-                joinedLoops,
-              });
-              if (success) {
-                Alert.alert("Success!", "Loop preferences successfully saved!");
-                //actions.resetForm();
-              }
-            }}
-          >
-            {(props) => (
-              <>
-                <Text style={globalStyles.titleText}></Text>
-                <Text style={globalStyles.titleText}>
-                  Please select 1 area of interest.
-                </Text>
+      <ImageBackground
+        source={{
+          uri: "https://img.freepik.com/free-photo/gray-abstract-wireframe-technology-background_53876-101941.jpg?size=626&ext=jpg",
+        }}
+        resizeMode="cover"
+        style={{ width: "100%", height: "100%" }}
+      >
+        <SafeAreaView style={globalStyles.container}>
+          <View style={{ alignItems: "center" }}>
+            <Formik
+              initialValues={{
+                Sports: false,
+                Music: false,
+                Volunteer: false,
+                Game: false,
+                Social: false,
+                Arts: false,
+                Outdoors: false,
+                Academic: false,
+                Media: false,
+              }}
+              //validationSchema={setUserLoopsSchema}
+              onSubmit={(joinedLoops) => {
+                var success = setUserLoops({
+                  joinedLoops,
+                });
+                if (success) {
+                  Alert.alert(
+                    "Success!",
+                    "Loop preferences successfully saved!"
+                  );
+                  //actions.resetForm();
+                }
+              }}
+            >
+              {(props) => (
+                <>
+                  <Text style={globalStyles.titleText}></Text>
+                  <Text style={globalStyles.titleText}>
+                    Please select 1 area of interest.
+                  </Text>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Sports"
+                        checked={checkSports}
+                        onPress={() => {
+                          setCheckSports(!checkSports);
+                          props.setFieldValue("Sports", !checkSports);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Music"
+                        checked={checkMusic}
+                        onPress={() => {
+                          setCheckMusic(!checkMusic);
+                          props.setFieldValue("Music", !checkMusic);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Volunteer"
+                        checked={checkVolunteer}
+                        onPress={() => {
+                          setCheckVolunteer(!checkVolunteer);
+                          props.setFieldValue("Volunteer", !checkVolunteer);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Game"
+                        checked={checkGame}
+                        onPress={() => {
+                          setCheckGame(!checkGame);
+                          props.setFieldValue("Game", !checkGame);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Social"
+                        checked={checkSocial}
+                        onPress={() => {
+                          setCheckSocial(!checkSocial);
+                          props.setFieldValue("Social", !checkSocial);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Arts"
+                        checked={checkArts}
+                        onPress={() => {
+                          setCheckArts(!checkArts);
+                          props.setFieldValue("Arts", !checkArts);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Outdoors"
+                        checked={checkOutdoors}
+                        onPress={() => {
+                          setCheckOutdoors(!checkOutdoors);
+                          props.setFieldValue("Outdoors", !checkOutdoors);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Academic"
+                        checked={checkAcademic}
+                        onPress={() => {
+                          setCheckAcademic(!checkAcademic);
+                          props.setFieldValue("Academic", !checkAcademic);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <View style={{ flexDirection: "row" }}>
+                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
+                      <CheckBox
+                        left
+                        title="Media"
+                        checked={checkMedia}
+                        onPress={() => {
+                          setCheckMedia(!checkMedia);
+                          props.setFieldValue("Media", !checkMedia);
+                        }}
+                      />
+                    </View>
+                  </View>
+                  <Text style={globalStyles.errorText}>
+                    {props.touched.eventLoop && props.errors.eventLoop}
+                  </Text>
 
-                <View style={{ flexDirection: "row" }}>
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Sports"
-                      checked={checkSports}
-                      onPress={() => {
-                        setCheckSports(!checkSports);
-                        props.setFieldValue("Sports", !checkSports);
+                  <View />
+                  <View>
+                    <Button
+                      title="Save Preferences"
+                      titleStyle={{ fontSize: 26, color: "black" }}
+                      buttonStyle={{
+                        borderWidth: 10,
+                        borderWidth: 1,
+                        borderColor: "black",
+                        titleColor: "black",
+                        backgroundColor: "#ffa835",
                       }}
+                      style={{ padding: 45 }}
+                      onPress={props.handleSubmit}
+                      // onPress={() => {
+                      //   console.log(props.values);
+                      // }}
                     />
                   </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Music"
-                      checked={checkMusic}
-                      onPress={() => {
-                        setCheckMusic(!checkMusic);
-                        props.setFieldValue("Music", !checkMusic);
-                      }}
-                    />
-                  </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Volunteer"
-                      checked={checkVolunteer}
-                      onPress={() => {
-                        setCheckVolunteer(!checkVolunteer);
-                        props.setFieldValue("Volunteer", !checkVolunteer);
-                      }}
-                    />
-                  </View>
-                </View>
-
-                <View style={{ flexDirection: "row" }}>
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Game"
-                      checked={checkGame}
-                      onPress={() => {
-                        setCheckGame(!checkGame);
-                        props.setFieldValue("Game", !checkGame);
-                      }}
-                    />
-                  </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Social"
-                      checked={checkSocial}
-                      onPress={() => {
-                        setCheckSocial(!checkSocial);
-                        props.setFieldValue("Social", !checkSocial);
-                      }}
-                    />
-                  </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Arts"
-                      checked={checkArts}
-                      onPress={() => {
-                        setCheckArts(!checkArts);
-                        props.setFieldValue("Arts", !checkArts);
-                      }}
-                    />
-                  </View>
-                </View>
-                <View style={{ flexDirection: "row" }}>
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Outdoors"
-                      checked={checkOutdoors}
-                      onPress={() => {
-                        setCheckOutdoors(!checkOutdoors);
-                        props.setFieldValue("Outdoors", !checkOutdoors);
-                      }}
-                    />
-                  </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Academic"
-                      checked={checkAcademic}
-                      onPress={() => {
-                        setCheckAcademic(!checkAcademic);
-                        props.setFieldValue("Academic", !checkAcademic);
-                      }}
-                    />
-                  </View>
-
-                  <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                    <CheckBox
-                      center
-                      title="Media"
-                      checked={checkMedia}
-                      onPress={() => {
-                        setCheckMedia(!checkMedia);
-                        props.setFieldValue("Media", !checkMedia);
-                      }}
-                    />
-                  </View>
-                </View>
-
-                <Text style={globalStyles.errorText}>
-                  {props.touched.eventLoop && props.errors.eventLoop}
-                </Text>
-
-                <View />
-                <View>
-                  <Button
-                    title="Save Preferences"
-                    titleStyle={{ fontSize: 26, color: "black" }}
-                    buttonStyle={{
-                      borderWidth: 1,
-                      borderColor: "black",
-                      titleColor: "black",
-                      backgroundColor: "#ff7b00",
-                    }}
-                    style={{ padding: 45 }}
-                    onPress={props.handleSubmit}
-                    // onPress={() => {
-                    //   console.log(props.values);
-                    // }}
-                  />
-                </View>
-              </>
-            )}
-          </Formik>
-        </View>
-      </SafeAreaView>
-      {/* </ImageBackground> */}
+                </>
+              )}
+            </Formik>
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
     </View>
   );
 }
