977b0db783271df9d57950fddc8b950e769b9ae0
Alec Chappell <32017824+alecclyde@users.noreply.github.com>
2022-01-15 21:21:24 -0500

    Changed userEventPreferences to 2 column layout

--- screens/userEventPreferences.js
+++ screens/userEventPreferences.js
@@ -14,7 +14,10 @@ import * as yup from "yup";
 import { Formik } from "formik";
 import { setUserLoops } from "../shared/firebaseMethods";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { Dimensions } from "react-native";
 
+const windowWidth = Dimensions.get("window").width;
+const windowHight = Dimensions.get("window").height;
 // const setUserLoopsSchema = yup.object({
 //   joinedLoops: yup.array.required("Please select at least 1 interest."),
 // });
@@ -31,14 +34,7 @@ export default function UserEventPreferences({ navigation }) {
   const [checkMedia, setCheckMedia] = useState(false);
 
   return (
-    <View
-      style={{
-        //borderColor: "#ffa835",
-        borderWidth: 7,
-        flex: 1,
-        backgroundColor: "#F8F8F8",
-      }}
-    >
+    <View>
       <ImageBackground
         source={{
           uri: "https://img.freepik.com/free-photo/gray-abstract-wireframe-technology-background_53876-101941.jpg?size=626&ext=jpg",
@@ -74,176 +70,182 @@ export default function UserEventPreferences({ navigation }) {
             >
               {(props) => (
                 <>
-                  <Text style={globalStyles.titleText}></Text>
-                  <Text style={globalStyles.titleText}>
-                    Please select at least 1 area of interest.
-                  </Text>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Sports"
-                        checkedIcon="futbol-o"
-                        uncheckedIcon="futbol-o"
-                        checkedColor="#ffa835"
-                        checked={checkSports}
-                        onPress={() => {
-                          setCheckSports(!checkSports);
-                          props.setFieldValue("Sports", !checkSports);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        checkedIcon="music"
-                        uncheckedIcon="music"
-                        checkedColor="#ffa835"
-                        title="Music"
-                        checked={checkMusic}
-                        onPress={() => {
-                          setCheckMusic(!checkMusic);
-                          props.setFieldValue("Music", !checkMusic);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Volunteer"
-                        checkedColor="#ffa835"
-                        checkedIcon="plus"
-                        uncheckedIcon="plus"
-                        checked={checkVolunteer}
-                        onPress={() => {
-                          setCheckVolunteer(!checkVolunteer);
-                          props.setFieldValue("Volunteer", !checkVolunteer);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Game"
-                        checkedIcon="gamepad"
-                        uncheckedIcon="gamepad"
-                        checkedColor="#ffa835"
-                        checked={checkGame}
-                        onPress={() => {
-                          setCheckGame(!checkGame);
-                          props.setFieldValue("Game", !checkGame);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Social"
-                        checkedIcon="users"
-                        uncheckedIcon="users"
-                        checkedColor="#ffa835"
-                        checked={checkSocial}
-                        onPress={() => {
-                          setCheckSocial(!checkSocial);
-                          props.setFieldValue("Social", !checkSocial);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Arts"
-                        checkedIcon="paint-brush"
-                        uncheckedIcon="paint-brush"
-                        checkedColor="#ffa835"
-                        checked={checkArts}
-                        onPress={() => {
-                          setCheckArts(!checkArts);
-                          props.setFieldValue("Arts", !checkArts);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Outdoors"
-                        checkedIcon="pagelines"
-                        uncheckedIcon="pagelines"
-                        checkedColor="#ffa835"
-                        checked={checkOutdoors}
-                        onPress={() => {
-                          setCheckOutdoors(!checkOutdoors);
-                          props.setFieldValue("Outdoors", !checkOutdoors);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Academic"
-                        checkedIcon="book"
-                        uncheckedIcon="book"
-                        checkedColor="#ffa835"
-                        checked={checkAcademic}
-                        onPress={() => {
-                          setCheckAcademic(!checkAcademic);
-                          props.setFieldValue("Academic", !checkAcademic);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <View style={{ flexDirection: "row" }}>
-                    <View style={{ flex: 1, paddingLeft: 5, paddingRight: 5 }}>
-                      <CheckBox
-                        left
-                        title="Media"
-                        checkedIcon="camera"
-                        uncheckedIcon="camera"
-                        checkedColor="#ffa835"
-                        checked={checkMedia}
-                        onPress={() => {
-                          setCheckMedia(!checkMedia);
-                          props.setFieldValue("Media", !checkMedia);
-                        }}
-                      />
-                    </View>
-                  </View>
-                  <Text style={globalStyles.errorText}>
-                    {props.touched.eventLoop && props.errors.eventLoop}
-                  </Text>
-
-                  <View />
                   <View>
-                    <Button
-                      title="Save Preferences"
-                      titleStyle={{ fontSize: 26, color: "black" }}
-                      buttonStyle={{
-                        borderWidth: 10,
-                        borderWidth: 1,
-                        borderColor: "black",
-                        titleColor: "black",
-                        backgroundColor: "#ffa835",
-                      }}
-                      style={{ padding: 45 }}
-                      onPress={props.handleSubmit}
-                      // onPress={() => {
-                      //   console.log(props.values);
-                      // }}
-                    />
+                    <Text style={globalStyles.titleText}></Text>
+                    <Text style={globalStyles.titleText}>
+                      Please select at least 1 area of interest.
+                    </Text>
+                    <View style={styles.container}>
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Sports"
+                          // title={<Text style={{}}>Sports</Text>}
+                          //textStyle={(size = 30)}
+                          checkedIcon="futbol-o"
+                          uncheckedIcon="futbol-o"
+                          checkedColor="#2B7D9C"
+                          checked={checkSports}
+                          onPress={() => {
+                            setCheckSports(!checkSports);
+                            props.setFieldValue("Sports", !checkSports);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          checkedIcon="music"
+                          uncheckedIcon="music"
+                          checkedColor="#2B7D9C"
+                          title="Music"
+                          checked={checkMusic}
+                          onPress={() => {
+                            setCheckMusic(!checkMusic);
+                            props.setFieldValue("Music", !checkMusic);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Volunteer"
+                          checkedColor="#2B7D9C"
+                          checkedIcon="plus"
+                          uncheckedIcon="plus"
+                          checked={checkVolunteer}
+                          onPress={() => {
+                            setCheckVolunteer(!checkVolunteer);
+                            props.setFieldValue("Volunteer", !checkVolunteer);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Game"
+                          checkedIcon="gamepad"
+                          uncheckedIcon="gamepad"
+                          checkedColor="#2B7D9C"
+                          checked={checkGame}
+                          onPress={() => {
+                            setCheckGame(!checkGame);
+                            props.setFieldValue("Game", !checkGame);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Social"
+                          checkedIcon="users"
+                          uncheckedIcon="users"
+                          checkedColor="#2B7D9C"
+                          checked={checkSocial}
+                          onPress={() => {
+                            setCheckSocial(!checkSocial);
+                            props.setFieldValue("Social", !checkSocial);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Arts"
+                          checkedIcon="paint-brush"
+                          uncheckedIcon="paint-brush"
+                          checkedColor="#2B7D9C"
+                          checked={checkArts}
+                          onPress={() => {
+                            setCheckArts(!checkArts);
+                            props.setFieldValue("Arts", !checkArts);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Outdoors"
+                          checkedIcon="pagelines"
+                          uncheckedIcon="pagelines"
+                          checkedColor="#2B7D9C"
+                          checked={checkOutdoors}
+                          onPress={() => {
+                            setCheckOutdoors(!checkOutdoors);
+                            props.setFieldValue("Outdoors", !checkOutdoors);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          title="Academic"
+                          checkedIcon="book"
+                          uncheckedIcon="book"
+                          checkedColor="#2B7D9C"
+                          checked={checkAcademic}
+                          onPress={() => {
+                            setCheckAcademic(!checkAcademic);
+                            props.setFieldValue("Academic", !checkAcademic);
+                          }}
+                        />
+                      </View>
+                      {/* </View>
+                  <View style={styles.container}> */}
+                      <View style={styles.boxContainer}>
+                        <CheckBox
+                          size={40}
+                          fontSize="80"
+                          title="Media"
+                          checkedIcon="camera"
+                          uncheckedIcon="camera"
+                          checkedColor="#2B7D9C"
+                          checked={checkMedia}
+                          onPress={() => {
+                            setCheckMedia(!checkMedia);
+                            props.setFieldValue("Media", !checkMedia);
+                          }}
+                        />
+                      </View>
+                    </View>
+
+                    <Text style={globalStyles.errorText}>
+                      {props.touched.eventLoop && props.errors.eventLoop}
+                    </Text>
+
+                    <View />
+                    <View>
+                      <Button
+                        title="Save Preferences"
+                        titleStyle={{ fontSize: 26, color: "black" }}
+                        buttonStyle={{
+                          borderWidth: 10,
+                          borderWidth: 1,
+                          borderColor: "black",
+                          titleColor: "black",
+                          backgroundColor: "#2B7D9C",
+                        }}
+                        style={{ flex: 1 }}
+                        onPress={props.handleSubmit}
+                        // onPress={() => {
+                        //   console.log(props.values);
+                        // }}
+                      />
+                    </View>
                   </View>
                 </>
               )}
@@ -267,8 +269,9 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "flex-start", // if you want to fill rows left to right
   },
   checkboxContainer: {
     flexDirection: "row",
@@ -283,6 +286,10 @@ const styles = StyleSheet.create({
   },
   checkbox: {
     alignSelf: "center",
+  },
+  boxContainer: {
+    width: "50%",
+    height: "20%",
   },
 });
 
