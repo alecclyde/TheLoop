826d3b1b5b00c58f0f797a9cfc79305d0148a0b7
Jmtonnies <jmtonnies@gmail.com>
2022-01-19 16:53:17 -0500

    Update searchPage.js

--- screens/searchPage.js
+++ screens/searchPage.js
@@ -1,8 +1,8 @@
 // import { StyleSheet, View, Text, Dimensions, Searchbar } from "react-native";
 // import MapView, { Marker } from "react-native-maps";
-// import { FontAwesome } from "@expo/vector-icons";
 // import * as Location from "expo-location";
 
+import { FontAwesome } from "@expo/vector-icons";
 import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
@@ -10,25 +10,31 @@ import {
   ImageBackground,
   View,
   FlatList,
-  TouchableOpacity, 
+  TouchableOpacity,
 } from "react-native";
-import { SearchBar } from "react-native-elements";
+import MapView, { Marker } from "react-native-maps";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+import { SearchBar, withTheme } from "react-native-elements";
 import { useIsFocused } from "@react-navigation/native";
 import * as firebase from "firebase";
 import { LinearGradient } from "expo-linear-gradient";
 import { TouchableScale } from "react-native-touchable-scale";
 import { Button, ListItem, Avatar } from "react-native-elements";
-import { globalStyles } from "../styles/global";
+// import { Dimensions } from "react-native";
+import { connect } from "react-redux";
 
 
+// const height = Dimensions.get("window").height * 0.3;
+// const width = Dimensions.get("window").width;
 
-
-
-export default function Search({ navigation }) {
+function Search(props, { navigation }) {
   const [events, setEvents] = useState([]);
-  const [search, setSearch] = useState({text: ''});
+  const [search, setSearch] = useState({ text: "" });
   const isFocused = useIsFocused();
-
+  const latitude = 41.241489;
+  const longitude = -77.041924;
+  const position = 0;
 
   //Gets all the events from the database and sets them to the events
   useEffect(() => {
@@ -40,137 +46,274 @@ export default function Search({ navigation }) {
       .then((snap) => {
         snap.docs.forEach((doc) => {
           if (doc.exists) {
-            setEvents((events) => [...events, {id: doc.id, loop: doc.data().loop, name: doc.data().name, creatorID: doc.data().creatorID}])
-          };
+            setEvents((events) => [
+              ...events,
+              {
+                id: doc.id,
+                loop: doc.data().loop,
+                name: doc.data().name,
+                creatorID: doc.data().creatorID,
+                address: doc.data().address,
+              },
+            ]);
+          }
         });
       });
   }, [isFocused]);
 
-
-
-
   return (
-    <View>
-        <SearchBar
-  placeholder="Type Here..."
-  onChangeText={(text)=> {setSearch({text})}}
-  value={search.text}
-/>
-      <ImageBackground
-        source={{
-          uri: "https://img.freepik.com/free-photo/gray-abstract-wireframe-technology-background_53876-101941.jpg?size=626&ext=jpg",
-        }}
-        resizeMode="cover"
-        style={{ width: "100%", height: "100%" }}
-      >
-        <FlatList
-          data={events}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => (
-            <TouchableOpacity
-              onPress={() =>
-                navigation.navigate("CardDetails", {
-                  id: item.id,
-                  loop: item.loop,
-                  name: item.name,
-                  creatorID: item.creatorID,
-                })
-              }
-            >
-              <ListItem
-                bottomDivide
-                bottomDivider={true}
-                Component={TouchableScale}
-                friction={90}
-                tension={100}
-                activeScale={0.95}
-                linearGradientProps={{
-                  colors: ["#FF9800", "#F44336"],
-                  start: { x: 1, y: 4 },
-                  end: { x: 0.2, y: 0 },
-                }}
-                ViewComponent={LinearGradient}
+    <ImageBackground
+      source={{
+        uri: "https://img.freepik.com/free-photo/gray-abstract-wireframe-technology-background_53876-101941.jpg?size=626&ext=jpg",
+      }}
+      resizeMode="cover"
+      style={{ width: "100%", height: "100%" }}
+    >
+      <View style={[styles.holder, { flexDirection: "column" }]}>
+        <View style={{ flex: 1 }}>
+          <SearchBar
+            placeholder="Type Here..."
+            onChangeText={(text) => {
+              setSearch({ text });
+            }}
+            value={search.text}
+          />
+        </View>
+        <View style={[styles.container, { flex: 2 }]}>
+          <FlatList
+            //contentContainerStyle={{ paddingBottom: }}
+            persistentScrollbar={true}
+            data={events}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <TouchableOpacity
+                style={styles.clickable}
+                onPress={() =>
+                  props.navigation.navigate("CardDetails", {
+                    id: item.id,
+                    loop: item.loop,
+                    name: item.name,
+                    creatorID: item.creatorID,
+                    address: item.address,
+                  })
+                }
               >
-                <ListItem.Content>
-                  <ListItem.Title style={globalStyles.listingItem}>
-                    {item.name}
-                  </ListItem.Title>
-                  <ListItem.Subtitle style={globalStyles.descriptionItem}>
-                    {item.loop}
-                  </ListItem.Subtitle>
-                </ListItem.Content>
-                <ListItem.Chevron color="white" />
-              </ListItem>
-            </TouchableOpacity>
-          )}
-        />
-      </ImageBackground>
-    </View>
+                <ListItem
+                  pad={16}
+                  bottomDivide
+                  Component={TouchableScale}
+                  button
+                  friction={90}
+                  tension={100} // These props are passed to the parent component (here TouchableScale)
+                  activeScale={0.95} //
+                  linearGradientProps={{
+                    colors: ["#2C2C2C", "#2C2C2C"],
+                    start: { x: 1, y: 0 },
+                    end: { x: 0.2, y: 0 },
+                  }}
+                  ViewComponent={LinearGradient}
+                >
+                  <Avatar
+                    size="large"
+                    //change this to either be icon of loop or that groups profile picture
+                    source={{
+                      uri: "https://business.twitter.com/content/dam/business-twitter/insights/may-2018/event-targeting.png.twimg.1920.png",
+                    }}
+                    resizeMode="cover"
+                    //style={{ width: "100%", height: "100%" }}
+                  />
+                  <ListItem.Content>
+                    <ListItem.Title style={styles.listingItem}>
+                      {item.name}
+                    </ListItem.Title>
+                    <ListItem.Subtitle style={styles.descriptionItem}>
+                      {item.loop}
+                    </ListItem.Subtitle>
+                    <ListItem.Subtitle style={styles.descriptionItem}>
+                      <Icon name="map-marker" size={16} color="white" />
+                      {"  "}
+                      {item.address}
+                    </ListItem.Subtitle>
+                  </ListItem.Content>
+                  <ListItem.Chevron color="gray" />
+                </ListItem>
+              </TouchableOpacity>
+            )}
+          />
+        </View>
+
+        <View style={[styles.container, { flex: 3 }]}>
+          <MapView
+            style={styles.mapStyle}
+            initialRegion={{
+              latitude: latitude,
+              longitude: longitude,
+              latitudeDelta: 0.04,
+              longitudeDelta: 0.05,
+            }}
+            customMapStyle={mapStyle}
+
+            //onPoiClick={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+          >
+            <Marker
+              draggable
+              coordinate={{
+                latitude: latitude,
+                longitude: longitude,
+              }}
+              onDragEnd={(e) => alert(JSON.stringify(e.nativeEvent.coordinate))}
+              title={"Messiah University"}
+              description={"position"}
+            />
+          </MapView>
+          <TouchableOpacity
+            style={styles.expander}
+            onPress={() => props.navigation.navigate("mapView")}
+          >
+            <Icon
+              //style={styles.icon}
+              //reverse
+              name="expand"
+              color="white"
+              size={20}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ImageBackground>
   );
 }
 
+const mapStyle = [
+  { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+  { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+  { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+  {
+    featureType: "administrative.locality",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry",
+    stylers: [{ color: "#263c3f" }],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#6b9a76" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [{ color: "#38414e" }],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#212a37" }],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#9ca5b3" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [{ color: "#746855" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [{ color: "#1f2835" }],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#f3d19c" }],
+  },
+  {
+    featureType: "transit",
+    elementType: "geometry",
+    stylers: [{ color: "#2f3948" }],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#d59563" }],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [{ color: "#17263c" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [{ color: "#515c6d" }],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.stroke",
+    stylers: [{ color: "#17263c" }],
+  },
+];
 
+const styles = StyleSheet.create({
+  container: {
+    color: "white",
+    margin: 5,
+  },
+  holder: {
+    flex: 1,
+    //padding: 0,
+  },
+  expander: {
+    width: 65,
+    height: 65,
+    justifyContent: "center",
+    alignItems: "center",
+    opacity: 0.6,
+    borderRadius: 75,
+    backgroundColor: "#696969",
+    left: 315,
+  },
+  mapStyle: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  paragraph: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
 
+    //marginTop: -50,
+  },
+  listingItem: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  descriptionItem: {
+    color: "white",
+  },
+});
 
-        
+const mapStateToProps = (state) => ({
+  events: state.events,
+});
 
+const mapDispatchToProps = (dispatch) => ({});
 
-
-
-
-
-
-
-
-
-
-
-
-// const MapComponent = () => {
-//   const [location, setLocation] = React.useState(null);
-//   const [error, setError] = React.useState(null);
-
-//   return (
-//     <View>
-//       {/* <Text style={styles.heading}>Map</Text> */}
-//       <MapView style={styles.map}>
-//         {location ? (
-//           <Marker coordinate={location} title="My location">
-//             <FontAwesome name="map-marker" size={40} color="#B12A5B" />
-//           </Marker>
-//         ) : (
-//           <Text>{error}</Text>
-//         )}
-//       </MapView>
-//     </View>
-//   );
-// };
-// export default MapComponent;
-
-// const styles = StyleSheet.create({
-//   map: {
-//     width: Dimensions.get("screen").width,
-//     height: Dimensions.get("screen").height * 0.9,
-//   },
-//   heading: {
-//     alignSelf: "center",
-//     paddingTop: 20,
-//     marginBottom: 10,
-//     fontSize: 24,
-//   },
-// });
-
-
-  // React.useEffect(() => {
-  //   (async () => {
-  //     let { status } = await Location.requestForegroundPermissionsAsync();
-  //     if (status !== "granted") {
-  //       setError("Permission to access location was denied");
-  //       return;
-  //     }
-  //     const locate = await Location.getCurrentPositionAsync({
-  //       accuracy: 6,
-  //     });
-  //     setLocation(locate.coords);
-  //   })();
-  // }, []);
+export default connect(mapStateToProps, mapDispatchToProps)(Search);
