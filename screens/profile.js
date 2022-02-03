cdfed3630c9939830301d254fea251d3a92cacef
Robbie Dorsey <airgo32@gmail.com>
2022-02-02 21:04:10 -0500

    Optimized event loading by storing event data per user

--- screens/profile.js
+++ screens/profile.js
@@ -40,7 +40,8 @@ function Profile(props, { navigation, route }) {
       setEmail(props.user.email);
       setFirstName(props.user.firstName);
       setLastName(props.user.LastName);
-      setEventIDs(props.user.myEvents);
+      setEvents(props.user.myEvents);
+      // setEventIDs(props.user.myEvents);
     }
   });
 
@@ -75,156 +76,143 @@ function Profile(props, { navigation, route }) {
   //   };
   // }, []);
 
-  useEffect(() => {
-    if (props.user) {
-      eventIDs.forEach((eventID) => {
-        getEventData(eventID).then((event) => {
+  // useEffect(() => {
+  //   if (props.user) {
+  //     eventIDs.forEach((eventID) => {
+  //       getEventData(eventID).then((event) => {
 
-          let creator;
+  //         let creator;
 
-          // SPRINT7: collapse this to just use event.creator
-          if (event.creator == undefined) {
-            creator = {userID: event.creatorID, userName: ""}
-          } else {
-            creator = event.creator
-          }
-          setEvents((events) => [
-            ...events,
-            {
-              id: eventID,
-              name: event.name,
-              loop: event.loop,
-              startDateTime: event.startDateTime,
-              creator: creator,
-              address: event.address,
-            }
-          ]);
-        });
-      });
-    }
-  },[eventIDs]);
+  //         // SPRINT7: collapse this to just use event.creator
+  //         if (event.creator == undefined) {
+  //           creator = {userID: event.creatorID, userName: ""}
+  //         } else {
+  //           creator = event.creator
+  //         }
+  //         setEvents((events) => [
+  //           ...events,
+  //           {
+  //             id: eventID,
+  //             name: event.name,
+  //             loop: event.loop,
+  //             startDateTime: event.startDateTime,
+  //             creator: creator,
+  //             address: event.address,
+  //           }
+  //         ]);
+  //       });
+  //     });
+  //   }
+  // },[eventIDs]);
   //console.log(props.user);
 
   return (
     <SafeAreaView style={globalStyles.container}>
-        <View style={globalStyles.header}>
-          <View style={styles.headerContent}>
-            <Image
-              style={globalStyles.avatar}
-              source={{
-                uri: "https://p.kindpng.com/picc/s/678-6789790_user-domain-general-user-avatar-profile-svg-hd.png",
-              }}
-            />
+      <View style={globalStyles.header}>
+        <View style={styles.headerContent}>
+          <Image
+            style={globalStyles.avatar}
+            source={{
+              uri: "https://p.kindpng.com/picc/s/678-6789790_user-domain-general-user-avatar-profile-svg-hd.png",
+            }}
+          />
 
-            <Text style={globalStyles.name}>
-              {firstName} {lastName}{" "}
-            </Text>
-            <Text style={globalStyles.userInfo}>{email} </Text>
+          <Text style={globalStyles.name}>
+            {firstName} {lastName}{" "}
+          </Text>
+          <Text style={globalStyles.userInfo}>{email} </Text>
+        </View>
+      </View>
+
+      <View style={styles.statusBar}>
+        <View style={styles.statusBarText}>
+          <View style={styles.statusBarTextLine}>
+            <Text style={styles.statusBarTextLine}>Followers</Text>
           </View>
         </View>
 
+        <View style={styles.verticleLine}></View>
+        <View style={styles.statusBarText}>
+          <Text style={styles.statusBarTextLine}>Following</Text>
+        </View>
+        <View style={styles.verticleLine}></View>
 
-          <View style={styles.statusBar}>
-            
-          <View style={styles.statusBarText}>
-            <View style={styles.statusBarTextLine}>
-              <Text style={styles.statusBarTextLine}>
-                Followers
-              </Text>
-            </View>
-          </View>
+        <View style={styles.statusBarText}>
+          <Text style={styles.statusBarTextLine}>Loops</Text>
+        </View>
+      </View>
 
-          <View style={styles.verticleLine}></View>
-            <View style={styles.statusBarText}>
-              <Text style={styles.statusBarTextLine}>
-                Following
-              </Text>
-            </View>
-          <View style={styles.verticleLine}></View>
+      <Text h3 style={styles.titles}>
+        Upcoming Events
+      </Text>
 
-          <View style={styles.statusBarText}>
-            <Text style={styles.statusBarTextLine}>
-              Loops
-            </Text>
-          </View>
-              
-          </View>
-
-
-
-
-            <Text h3 style={styles.titles}>
-              Upcoming Events
-            </Text>
-
-
-            
-
-            <ScrollView
-              persistentScrollbar={true}
-              horizontal={true}
-              style={{ flex: 1 }}
+      <ScrollView
+        persistentScrollbar={true}
+        horizontal={true}
+        style={{ flex: 1 }}
+      >
+        {events //upcoming events
+          .filter((item) => item.startDateTime > moment().unix())
+          .sort((item1, item2) => item1.startDateTime - item2.startDateTime)
+          .map((event) => (
+            <TouchableOpacity
+              style={styles.clickable}
+              key={event.id}
+              onPress={() =>
+                props.navigation.navigate("CardDetails", {
+                  id: event.id,
+                  name: event.name,
+                  loop: event.loop,
+                  creator: event.creator,
+                  startDateTime: event.startDateTime,
+                  address: event.address,
+                })
+              }
             >
-              {events.map((event) => (
-                <TouchableOpacity
-                  style={styles.clickable}
-                  key={event.id}
-                  onPress={() =>
-                    props.navigation.navigate("CardDetails", {
-                      id: event.id,
-                      name: event.name,
-                      loop: event.loop,
-                      creatorID: event.creatorID,
-                      startDateTime: event.startDateTime,
-                      address: event.address,
-                    })
-                  }
-                >
-                  <ListItem
-                    pad={16}
-                    bottomDivide={true}
-                    Component={TouchableScale}
-                    button
-                    friction={90}
-                    tension={100} // These props are passed to the parent component (here TouchableScale)
-                    activeScale={0.95} //
-                    linearGradientProps={{
-                      colors: ["#3B4046", "#3B4046"],
-                      start: { x: 1, y: 0 },
-                      end: { x: 0.2, y: 0 },
-                    }}
-                    ViewComponent={LinearGradient}
-                  >
-                    <Avatar
-                      size="large"
-                      //change this to either be icon of loop or that groups profile picture
-                      source={{
-                        uri: "https://business.twitter.com/content/dam/business-twitter/insights/may-2018/event-targeting.png.twimg.1920.png",
-                      }}
-                      resizeMode="cover"
-                      //style={{ width: "100%", height: "100%" }}
-                    />
-                    <ListItem.Content>
-                      <ListItem.Title style={styles.listingItem}>
-                        {event.name}
-                      </ListItem.Title>
-                      <ListItem.Subtitle style={styles.descriptionItem}>
-                        {event.loop}
-                      </ListItem.Subtitle>
-                      <ListItem.Subtitle style={styles.descriptionItem}>
-                        <Icon name="map-marker" size={16} color="white" />
-                        {"  "}
-                        {event.address}
-                      </ListItem.Subtitle>
-                    </ListItem.Content>
-                    <ListItem.Chevron color="gray" />
-                  </ListItem>
-                </TouchableOpacity>
-              ))}
-            </ScrollView>
+              <ListItem
+                pad={16}
+                bottomDivide={true}
+                Component={TouchableScale}
+                button
+                friction={90}
+                tension={100} // These props are passed to the parent component (here TouchableScale)
+                activeScale={0.95} //
+                linearGradientProps={{
+                  colors: ["#3B4046", "#3B4046"],
+                  start: { x: 1, y: 0 },
+                  end: { x: 0.2, y: 0 },
+                }}
+                ViewComponent={LinearGradient}
+              >
+                <Avatar
+                  size="large"
+                  //change this to either be icon of loop or that groups profile picture
+                  source={{
+                    uri: "https://business.twitter.com/content/dam/business-twitter/insights/may-2018/event-targeting.png.twimg.1920.png",
+                  }}
+                  resizeMode="cover"
+                  //style={{ width: "100%", height: "100%" }}
+                />
+                <ListItem.Content>
+                  <ListItem.Title style={styles.listingItem}>
+                    {event.name}
+                  </ListItem.Title>
+                  <ListItem.Subtitle style={styles.descriptionItem}>
+                    {event.loop}
+                  </ListItem.Subtitle>
+                  <ListItem.Subtitle style={styles.descriptionItem}>
+                    <Icon name="map-marker" size={16} color="white" />
+                    {"  "}
+                    {event.address}
+                  </ListItem.Subtitle>
+                </ListItem.Content>
+                <ListItem.Chevron color="gray" />
+              </ListItem>
+            </TouchableOpacity>
+          ))}
+      </ScrollView>
 
-        
-{/* 
+      {/* 
         <View style={{ backgroundColor: "black" }}>
           <Text h3 style={{ textAlign: "center", color: "#ffa835" }}>
             Upcoming Events
@@ -313,9 +301,9 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 5,
     paddingVertical: 37,
-    shadowOffset: {width: 1, height: .1},
+    shadowOffset: { width: 1, height: 0.1 },
     shadowOpacity: 0.8,
-    shadowColor: 'black',
+    shadowColor: "black",
   },
   statusBar: {
     justifyContent: "center",
@@ -324,21 +312,21 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     margin: 5,
     paddingVertical: 37,
-    shadowOffset: {width: 1, height: .1},
+    shadowOffset: { width: 1, height: 0.1 },
     shadowOpacity: 0.8,
-    shadowColor: 'black',
+    shadowColor: "black",
     alignSelf: "stretch",
     flexDirection: "row",
   },
 
-    titles: {
+  titles: {
     textAlign: "left",
     color: "white",
     marginLeft: 10,
-    fontFamily: 'Helvetica-Bold',
+    fontFamily: "Helvetica-Bold",
   },
 
-  statusBarText:{
+  statusBarText: {
     margin: 5,
     flex: 1,
     flexDirection: "row",
@@ -347,24 +335,20 @@ const styles = StyleSheet.create({
   },
 
   verticleLine: {
-    height: '100%',
+    height: "100%",
     width: 1,
-    backgroundColor: '#909090',
+    backgroundColor: "#909090",
     textAlign: "center",
   },
 
-  statusBarTextLine:{
+  statusBarTextLine: {
     flex: 1,
     flexDirection: "column",
     fontWeight: "bold",
     color: "white",
     textAlign: "center",
     fontSize: 20,
-  }
-
-
-
-
+  },
 });
 
 const mapStateToProps = (state) => ({
